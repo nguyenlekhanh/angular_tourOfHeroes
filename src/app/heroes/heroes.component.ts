d31442dc2d4dd423e61b4235625a101adbd15b37
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-07 17:55:50 -0600

    create simple hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common'; //ngFor
 import { FormsModule } from '@angular/forms'; // Import FormsModule
 import {HEROES} from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -17,9 +18,24 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
+
+  //xxx
+  constructor(private heroService: HeroService) {
+
+  }
+
+  //xxx
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
