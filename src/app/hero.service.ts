4ee3bf6e76c968637a759a1eb66d0bdb9f467595
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-07 21:10:45 -0600

    update hero function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -79,4 +79,16 @@ export class HeroService {
       return of(result as T);
     };
   }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 }
